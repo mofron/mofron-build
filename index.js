b28c5dc8d89c9c254ab9e23d9f87d46e30ce9af3
simpart <taria.ki4m@gmail.com>
2022-06-04 00:16:50 +0000

    add parameter

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const { exec } = require("child_process");
 
 /* convert mofron tag to js */
 console.log("*** convert mofron tag to javascript");
+console.log(process.argv.length);
+//if (3 === process.argv.length) {
+//    
+//} else {
+//    
+//}
+
 let tag_tgt  = (undefined !== process.argv[2]) ? process.argv[2] : "index";
+let cnf_tgt  = (undefined !== process.argv[3]) ? process.argv[3] : tag_tgt;
+if (("production" === cnf_tgt) || ("development" === cnf_tgt)) {
+    cnf_tgt  = tag_tgt;
+}
+
 let exec_cmd = "npx mofron-tag ./mof/" + tag_tgt + ".mf ./js/"+ tag_tgt +".js";
 console.log(exec_cmd);
 exec(exec_cmd, (err, stdout, stderr) => {
@@ -20,8 +32,12 @@ exec(exec_cmd, (err, stdout, stderr) => {
 let bundle = () => {
     /* bundle js */
     console.log("*** bundling javascript");
-    let mode = (undefined !== process.argv[3]) ? process.argv[3] : "development";
-    exec_cmd = "npx webpack --config " + __dirname + "/../../conf/webpack.config." + tag_tgt + ".js --mode " + mode;
+    let mode = process.argv[process.argv.length-1];
+    if (("production" !== mode) && ("development" !== mode)) {
+        mode = "development";
+    }
+
+    exec_cmd = "npx webpack --config " + __dirname + "/../../conf/webpack.config." + cnf_tgt + ".js --mode " + mode;
     console.log(exec_cmd);
     exec(exec_cmd, (err, stdout, stderr) => {
         if (0 < stderr.length) {
