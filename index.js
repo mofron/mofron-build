451ccdb0b8bf0a8fa7450bebb23ac22b7e48ddb1
EC2 Default User <ec2-user@ip-172-26-5-87.ap-northeast-1.compute.internal>
2024-02-07 05:51:25 +0000

    spa

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { exec } = require("child_process");
 let mofron = null;
 let mfout  = null;
 let config = null;
+let spa    = "";
 
 if (2 === process.argv.length) {
     mofron = "./mof/index.mf";
@@ -18,17 +19,19 @@ if (2 === process.argv.length) {
     const def_opt = [
         { name: 'mofron', alias: 'm', type: String },
         { name: 'mfout',  alias: 'o', type: String },
-        { name: 'config', alias: 'c', type: String }
+        { name: 'config', alias: 'c', type: String },
+	{ name: 'spa',    alias: 's', type: String },
     ];
     const options = cli_arg(def_opt);
     mofron = options.mofron;
     mfout  = options.mfout;
     config = options.config;
+    spa    = (undefined === options.spa) ? "" : "--spa";
 }
 
 /* convert mofron tag to js */
 console.log("*** convert mofron tag to javascript");
-let exec_cmd = "npx mofron-tag " + mofron + " " + mfout;
+let exec_cmd = "npx mofron-tag " + mofron + " " + mfout + " " + spa;
 console.log(exec_cmd);
 
 exec(exec_cmd, (err, stdout, stderr) => {
